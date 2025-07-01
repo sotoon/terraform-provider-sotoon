52ac8fe43a6331336396218617921a8327fb0b85
Moein Tavakoli <mtawakoli2283@gmail.com>
2025-07-02 01:36:05 +0330

    [ Feature ] Implement base terraform provider , add provider , vars

---
+++ examples/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    compute = {
+      source  = "sotoon/sotoon"
+      version = "1.0.0"
+    }
+  }
+}
+
+
+provider "sotoon" {
+  api_token     = var.api_token
+  workspace_id = var.workspace_id
+}
+
+
---
+++ examples/terraform.tfvars
@@ -0,0 +1,2 @@
+api_token     = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+workspace_id = "your-workspace-id"
