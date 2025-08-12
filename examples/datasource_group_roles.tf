00fbd2d7e6a6f0af6143704aba0e5346b999bef6
Moein Tavakoli <mtawakoli2283@gmail.com>
2025-08-12 14:20:26 +0330

    [ Feature ] Add datasource groups binding

---
+++ examples/datasource_group_roles.tf
@@ -0,0 +1,9 @@
+data "sotoon_iam_group_roles" "all" {
+  workspace_id = var.workspace_id_target
+  group_id     = "3f355c0a-7871-47e4-8dcc-2d752f55b720"
+}
+
+output "all_group_roles_list" {
+  description = "A list of all role names in the group."
+  value       = data.sotoon_iam_group_roles.all.roles.*.name
+}
