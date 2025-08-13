48bd6c6a6781e302d309a87382c13cdee5c7e0e9
Moein Tavakoli <mtawakoli2283@gmail.com>
2025-08-13 17:51:06 +0330

    [ Fix ] Update resource , datesource and using locals

--- examples/datasource_group_users.tf
+++ examples/datasource_group_users.tf
@@ -1,6 +1,6 @@
 data "sotoon_iam_group_users" "all" {
   workspace_id = var.workspace_id_target
-  group_id     = "3f355c0a-7871-47e4-8dcc-2d752f55b720"
+  group_id     = local.target_group_developer.id
 }
 
 output "all_group_users_list" {
