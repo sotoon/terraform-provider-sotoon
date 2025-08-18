bf488275d8bf999b74a64397e852a73cc5304a8a
Moein Tavakoli <mtawakoli2283@gmail.com>
2025-08-18 09:37:13 +0330

    [ Fix ] Update , reformat terraform codebase , using locals

--- examples/datasource_group_users_list.tf
+++ examples/datasource_group_users_list.tf
@@ -1,9 +1,9 @@
-data "sotoon_iam_group_users_list" "developer" {
+data "sotoon_iam_group_users" "developer" {
   workspace_id = var.workspace_id_target
   group_id     = local.target_group_developer.id
 }
 
-output "all_group_users_list" {
+output "all_group_developer_list" {
   description = "A list of all user names in the developer group."
-  value       = data.sotoon_iam_group_users_list.developer.users.*.name
+  value       = data.sotoon_iam_group_users.developer.users.*.name
 }
